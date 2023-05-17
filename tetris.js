c7b9a381f0d129ce60517d8c94ed361f241b8360
Mario Ponce <mario@mpondev.com>
2023-05-17 16:59:55 +0200

    Add top collision and colors

--- tetris.js
+++ tetris.js
@@ -32,38 +32,38 @@ function createPieces(type) {
     ];
   } else if (type === 'O') {
     return [
-      [1, 1],
-      [1, 1],
+      [2, 2],
+      [2, 2],
     ];
   } else if (type === 'L') {
     return [
-      [0, 1, 0],
-      [0, 1, 0],
-      [0, 1, 1],
+      [0, 3, 0],
+      [0, 3, 0],
+      [0, 3, 3],
     ];
   } else if (type === 'J') {
     return [
-      [0, 1, 0],
-      [0, 1, 0],
-      [1, 1, 0],
+      [0, 4, 0],
+      [0, 4, 0],
+      [4, 4, 0],
     ];
   } else if (type === 'I') {
     return [
-      [0, 1, 0, 0],
-      [0, 1, 0, 0],
-      [0, 1, 0, 0],
-      [0, 1, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
     ];
   } else if (type === 'S') {
     return [
-      [0, 1, 1],
-      [1, 1, 0],
+      [0, 6, 6],
+      [6, 6, 0],
       [0, 0, 0],
     ];
   } else if (type === 'Z') {
     return [
-      [1, 1, 0],
-      [0, 1, 1],
+      [7, 7, 0],
+      [0, 7, 7],
       [0, 0, 0],
     ];
   }
@@ -81,7 +81,7 @@ function drawPiece(piece, offset) {
   piece.forEach((x, y) => {
     x.forEach((value, x) => {
       if (value !== 0) {
-        context.fillStyle = 'red';
+        context.fillStyle = colors[value];
         context.fillRect(x + offset.x, y + offset.y, 1, 1);
       }
     });
@@ -121,6 +121,9 @@ function pieceReset() {
   pieceInfo.pos.y = 0;
   pieceInfo.pos.x =
     ((arena[0].length / 2) | 0) - ((pieceInfo.piece[0].length / 2) | 0);
+  if (collide(arena, pieceInfo)) {
+    arena.forEach(row => row.fill(0));
+  }
 }
 
 function pieceRotate(dir) {
@@ -168,6 +171,17 @@ function update(time = 0) {
   draw();
   requestAnimationFrame(update);
 }
+
+const colors = [
+  null,
+  '#FF0D72',
+  '#0DC2FF',
+  '#0DFF72',
+  '#F538FF',
+  '#FF8E0D',
+  '#FFE138',
+  '#3877FF',
+];
 
 const arena = createPiece(12, 20);
 
