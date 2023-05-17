982f8ae5a43ec3a0d9beaa2dd2d791de617cf6eb
Mario Ponce <mario@mpondev.com>
2023-05-17 15:59:00 +0200

    Add T-piece

--- tetris.js
+++ tetris.js
@@ -1,5 +1,22 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
+context.scale(20, 20);
+
 context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.clientWidth, canvas.height);
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+const piece = [
+  [0, 0, 0],
+  [1, 1, 1],
+  [0, 1, 0],
+];
+
+piece.forEach((x, y) => {
+  x.forEach((value, x) => {
+    if (value !== 0) {
+      context.fillStyle = 'red';
+      context.fillRect(x, y, 1, 1);
+    }
+  });
+});
