3fc4de7d8717ee8d38bcc2c77ffa1b72ea4e2e36
Mario Ponce <mario@mpondev.com>
2023-05-17 17:05:44 +0200

    Add erase-lines functionality

--- tetris.js
+++ tetris.js
@@ -3,6 +3,19 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
+function arenaSweep() {
+  outer: for (let y = arena.length - 1; y > 0; y--) {
+    for (let x = 0; x < arena[y].length; x++) {
+      if (arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+    const row = arena.splice(y, 1)[0].fill(0);
+    arena.unshift(row);
+    y++;
+  }
+}
+
 function collide(arena, pieceInfo) {
   const [m, p] = [pieceInfo.piece, pieceInfo.pos];
   for (let y = 0; y < m.length; y++) {
@@ -104,6 +117,7 @@ function pieceDrop() {
     pieceInfo.pos.y--;
     merge(arena, pieceInfo);
     pieceReset();
+    arenaSweep();
   }
   dropCounter = 0;
 }
