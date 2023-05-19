99a353260b7d8a97807cb3bc81acd410b5d88cae
mpondev <mario@mpondev.com>
2023-05-19 09:39:07 +0200

    Finish useGameStats hook

--- tetris-react/src/hooks/useGameStats.js
+++ tetris-react/src/hooks/useGameStats.js
@@ -1,10 +1,18 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback } from 'react';
 
 function buildGameStats() {
-  {
+  return {
     level: 1,
     linesCompleted: 0,
     linesPerLevel: 10,
     points: 0,
-  }
+  };
+}
+
+export function useGameStats() {
+  const [gameStats, useGameStats] = useState(buildGameStats());
+
+  const addLinesCleared = useCallback(() => {}, []);
+
+  return [gameStats, addLinesCleared];
 }
